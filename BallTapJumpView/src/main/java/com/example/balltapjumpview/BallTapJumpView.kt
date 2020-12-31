20544e157834561d7cf5b51dc5282ccbab238463
Anwesh43 <anweshthecool0@gmail.com>
2021-01-01 02:40:24 +0530

    adding Int and Float extensiion methods

--- BallTapJumpView/src/main/java/com/example/balltapjumpview/BallTapJumpView.kt
+++ BallTapJumpView/src/main/java/com/example/balltapjumpview/BallTapJumpView.kt
@@ -23,3 +23,7 @@ val sizeFactor : Float = 5.9f
 val strokeFactor : Float = 90f
 val parts : Int = 4
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float  = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
