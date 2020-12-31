d3f690c82ddf7c70bf64fe04c6a22185cd61fa7f
Anwesh43 <anweshthecool0@gmail.com>
2021-01-01 02:37:35 +0530

    adding Canvas extension methods

--- BallTapJumpView/src/main/java/com/example/balltapjumpview/BallTapJumpView.kt
+++ BallTapJumpView/src/main/java/com/example/balltapjumpview/BallTapJumpView.kt
@@ -1,2 +1,25 @@
 package com.example.balltapjumpview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#009688",
+    "#FF9800",
+    "#795548",
+    "#03A9F4"
+).map {
+   Color.parseColor(it)
+}.toTypedArray()
+val delay : Long = 20
+val sizeFactor : Float = 5.9f
+val strokeFactor : Float = 90f
+val parts : Int = 4
+
